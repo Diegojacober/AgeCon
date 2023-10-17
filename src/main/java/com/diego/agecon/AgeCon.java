76603050e70873bf2609a01955d626e4f66328c7
ETS <ct67ca@bosch.com>
2023-10-17 10:21:11 -0300

    Criando projeto

---
+++ src/main/java/com/diego/agecon/AgeCon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.diego.agecon;
+
+/**
+ *
+ * @author ct67ca
+ */
+public class AgeCon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
