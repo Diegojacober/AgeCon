46c1d4bc1799898d655a12425cf2b48f5d6c24a0
ETS <ct67ca@bosch.com>
2023-10-17 10:38:07 -0300

    Criando menu

--- src/main/java/com/diego/agecon/view/Principal.java
+++ src/main/java/com/diego/agecon/view/Principal.java
@@ -3,19 +3,37 @@ package com.diego.agecon.view;
 import javax.swing.JFrame;
 
 public class Principal extends javax.swing.JFrame {
-    
+
     public Principal() {
         initComponents();
         setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
-    
+
+    /*é uma anotação em Java que informa ao compilador para suprimir avisos
+    específicos gerados durante a compilação do código.*/
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jmbBarraMenu = new javax.swing.JMenuBar();
+        jmArquivo = new javax.swing.JMenu();
+        jmSobre = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("AGECON - Agenda de Contatos");
+        setBackground(new java.awt.Color(102, 102, 102));
+        setForeground(new java.awt.Color(102, 102, 102));
         setResizable(false);
+
+        jmArquivo.setMnemonic('a');
+        jmArquivo.setText("Arquivo");
+        jmbBarraMenu.add(jmArquivo);
+
+        jmSobre.setMnemonic('s');
+        jmSobre.setText("Sobre");
+        jmbBarraMenu.add(jmSobre);
+
+        setJMenuBar(jmbBarraMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -25,7 +43,7 @@ public class Principal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGap(0, 577, Short.MAX_VALUE)
         );
 
         setSize(new java.awt.Dimension(816, 608));
@@ -41,5 +59,8 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jmArquivo;
+    private javax.swing.JMenu jmSobre;
+    private javax.swing.JMenuBar jmbBarraMenu;
     // End of variables declaration//GEN-END:variables
 }
